388a8e19bee6510e7b8558994a46d119c9ecf5bf
REÇ <ecoban@sutas.com.tr>
2019-05-28 16:22:32 +0300

    you can learn how to use parent func in parent

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -51,6 +51,10 @@ class PickLocation extends Component {
                 locationChosen: true
             };
         });
+        this.props.onLocationPick({
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        });
     }
 
     render () {
--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -4,10 +4,27 @@ import { View, Text } from "react-native";
 import PickLocation from "../../components/PickLocation/PickLocation";
 
 class MainScreen extends Component {
+    state = {
+        location: {
+            value: null
+        }
+    }
+
+    locationPickedHandler = location => {
+        this.setState(prevState => {
+            return {
+                location: {
+                    ...prevState.location,
+                    value: location
+                }
+            }
+        });
+    }
+
     render() {
         return (
             <View style={{width: "100%", height: "100%"}}>
-                <PickLocation />
+                <PickLocation onLocationPick={this.locationPickedHandler}/>
             </View>
         );
     }
