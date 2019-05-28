5e1d41e988c45e72dafbbee02e19721588adfff5
REÇ <ecoban@sutas.com.tr>
2019-05-28 14:17:43 +0300

    picking a location and user

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -12,17 +12,64 @@ class PickLocation extends Component {
                 Dimensions.get("window").width / 
                 Dimensions.get("window").height * 
                 0.0122
-        }
+        },
+        locationChosen: false
     }
+
+    getLocationHandler = () => {
+        navigator.geolocation.getCurrentPosition(pos => {
+            const coordsEvent = {
+                nativeEvent: {
+                    coordinate: {
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude
+                    }
+                }
+            };
+            this.pickLocationHandler(coordsEvent);
+        },
+        err => {
+            alert("Konum alinamadi");
+        });
+    }
+
+    pickLocationHandler = event => {
+        const coords = event.nativeEvent.coordinate;
+        this.map.animateToRegion({
+            ...this.state.focusedLocation,
+            latitude: coords.latitude,
+            longitude: coords.longitude
+        });
+
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                },
+                locationChosen: true
+            };
+        });
+    }
+
     render () {
+        let marker = null;
+        if (this.state.locationChosen) {
+            marker = <MapView.Marker coordinate={this.state.focusedLocation}/>;
+        }
         return (
             <View style={styles.container}>
                 <MapView
                     initialRegion={this.state.focusedLocation}
+                    // region={this.state.focusedLocation}
                     style={styles.map}
-                    />
+                    onPress={this.pickLocationHandler}
+                    ref={ref => this.map = ref}
+                    >{marker}
+                    </MapView>
                 <View style={styles.button}>
-                    <Button title="Locate me" onPress={() => alert("test")}/>
+                    <Button title="Locate me" onPress={this.getLocationHandler}/>
                 </View>
             </View>
         );
--- src/screens/MainScreen/MainScreen.js
+++ src/screens/MainScreen/MainScreen.js
@@ -7,9 +7,7 @@ class MainScreen extends Component {
     render() {
         return (
             <View style={{width: "100%", height: "100%"}}>
-                <Text>TEST</Text>
                 <PickLocation />
-                <Text>TEST</Text>
             </View>
         );
     }
